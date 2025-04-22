2e335c1473d159120c5ccda7ae70e26bb5349f39
Sayan880 <124447318+Sayan880@users.noreply.github.com>
2025-04-22 21:23:42 +0200

    UI Part 2

---
+++ My project/Assets/Scripts/LobbyUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class LobbyUI : MonoBehaviour
+{
+    [SerializeField] InputField joinInput;
+    [SerializeField] Button joinButton;
+    [SerializeField] Button hostButton;
+
+    public void Host(){
+        joinInput.interactable = false;
+        joinButton.interactable = false;
+        hostButton.interactable = false;
+    }
+
+    public void Join(){
+        joinInput.interactable = false;
+        joinButton.interactable = false;
+        hostButton.interactable = false;
+    }
+}
