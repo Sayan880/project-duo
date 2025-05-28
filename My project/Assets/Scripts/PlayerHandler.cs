38a474844fcf9d3b8e4f5d9b8ccdf1ad0d48e0c6
Sayan880 <124447318+Sayan880@users.noreply.github.com>
2025-05-28 09:54:34 +0200

    bruche player prefab

---
+++ My project/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using Unity.Netcode;
+public class PlayerHandler : MonoBehaviour
+{
+    public GameObject cam;
+    public Material pm;
+    public PlayerMovement move;
+    public NetworkVariable<int> PlayerID = new NetworkVariable<int>();
+    public static int pCount;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            PlayerID.Value = pCount;
+            pCount++;
+        }
+
+        if (IsLocalPlayer)
+        {
+            move.enabled = true;
+            GetComponent<Renderer>().material = Material.Instantiate(pm);
+            GetComponent<Renderer>().material.color = Color.green;
+            cam.SetActive(true);
+
+        }
+        else
+        {
+            Destroy(move);
+            Destroy(cam);
+            GetComponent<Renderer>().material = Material.Instantiate(pm);
+            GetComponent<Renderer>().material.color = Color.red;
+
+        }
+    }
+
+    public override void OnDestroy()
+    {
+        NetworkObject.Despawn(true);
+        
+    }
+    public override void OnNetworkDespawn()
+    {
+        NetworkObject.Despawn();
+    }
+}
