aa27683585bba27d20cc6e28e20087c594a1cc4e
Ivan <ivansimi090@gmail.com>
2025-05-31 16:26:23 +0200

    movement, aber alles stuck

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-
-    // Update is called once per frame
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _jumpforce = 200;
+    [SerializeField] private Rigidbody _rb;
     void Update()
     {
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        dir *= Time.deltaTime * speed;
-        transform.position += dir;
+        var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
+        vel.y = _rb.angularVelocity.y;
+        _rb.angularVelocity = vel;
+
+        if (Input.GetKeyDown(KeyCode.Space)) _rb.AddForce(Vector3.up * _jumpforce);
     }
 }
