6aa418456c7f0fab018637c4323e0e76bcf73ca4
Ivan <ivansimi090@gmail.com>
2025-06-01 17:49:18 +0200

    physics fixe

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1;
-    [SerializeField] private float _jumpforce = 200;
-    [SerializeField] private Rigidbody _rb;
-    void Update()
-    {
-        var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
-        vel.y = _rb.angularVelocity.y;
-        _rb.angularVelocity = vel;
+    public float speed = 5f;
+    Rigidbody rb;
 
-        if (Input.GetKeyDown(KeyCode.Space)) _rb.AddForce(Vector3.up * _jumpforce);
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(h, 0f, v) * speed;
+        rb.MovePosition(rb.position + move * Time.fixedDeltaTime);
     }
 }
