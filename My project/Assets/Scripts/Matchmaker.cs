f17decaa07d7da9c7696bd863e619cbdc98ae7f8
Sayan880 <124447318+Sayan880@users.noreply.github.com>
2025-06-02 11:26:07 +0200

    njeriiiii

--- My project/Assets/Scripts/Matchmaker.cs
+++ My project/Assets/Scripts/Matchmaker.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using TMPro;
 
 public class Matchmaker : MonoBehaviour
 {
+
+    public TMP_InputField JoinKeyInput, ShowJoinKey;
+    public GameObject WaitHost, WaitClient;
+    public TextMeshProUGUI playerCount;
+    public TMP_InputField lobbyNameInput, DebugLog;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
